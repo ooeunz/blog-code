9b76554f054aefe3c0a6a14ce2635eb804d4c09d
ooeunz <yuns994@gmail.com>
2021-03-18 16:04:44 +0900

    Modify common name in certificate

--- devops/kubernetes/monitoring/script/start-monitoring.sh
+++ devops/kubernetes/monitoring/script/start-monitoring.sh
@@ -5,5 +5,6 @@ echo 🎉 Deploy monitoring system
 kubectl create namespace monitoring
 
 kubectl apply -f ../prometheus
+kubectl apply -f ../alertmanager
 kubectl apply -f ../node-exporter
 kubectl apply -f ../kube-state-metrics
